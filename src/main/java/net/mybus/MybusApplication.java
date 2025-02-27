e781ed4e3f8b0659a45788be3b328f7d567c1063
kangkamchan <topthanks@naver.com>
2025-02-28 08:46:55 +0900

    [ìfirst commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/net/mybus/MybusApplication.java
@@ -0,0 +1,13 @@
+package net.mybus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mybus/MybusApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mybus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
