65c452bf1469aad3dec5d18845342304032aeece
kangkamchan <topthanks@naver.com>
2025-02-28 22:11:04 +0900

    [추가] ã메인 페이지 html, 컨트롤러

---
+++ src/main/java/net/mybus/controller/MyBusController.java
@@ -0,0 +1,14 @@
+package net.mybus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bus")
+public class MyBusController {
+    @GetMapping("/")
+    public String myBus() {
+        return "main";
+    }
+}
