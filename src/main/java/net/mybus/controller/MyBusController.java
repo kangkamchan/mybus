305cba64f9e0295ddce64a9f33f384e76d615601
kangkamchan <topthanks@naver.com>
2025-03-10 15:17:43 +0900

    [수정] 디렉ítemplates 리 구조 변경

--- src/main/java/net/mybus/controller/MyBusController.java
+++ src/main/java/net/mybus/controller/MyBusController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyBusController {
     @GetMapping
     public String myBus() {
-        return "index";
+        return "bus/index";
     }
 }
